e8add5f1f528f791c139d64487bd87d504f8794f
FujiwaraNaoto <naotofujiwara890@gmail.com>
2023-03-23 17:13:16 +0900

    add sugoroku

--- sugoroku.cpp
+++ sugoroku.cpp
@@ -25,6 +25,34 @@ int main(){
 
     return 0;
 }
+/*
+
+
+//[g,x,y], ax+by=g
+std::tuple<long long,long long ,long long> extgcd(long long a,long long b){
+    
+    long long d=a;
+
+    if(b!=0){
+
+        auto [d,x,y]=extgcd(b,a%b);
+        return std::make_tuple(d,y,x-(a/b)*y);
+
+    }else{
+
+        return std::make_tuple(d,1,0);
+    }
+
+}
+
+
+
+
+
+
+
+*/
+
 
 
 /*
